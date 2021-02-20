61fcc3ea0adbc7bbe882b6ca13738952fb0cd29f
Dan Kreiger <dan@dankreiger.com>
2021-02-20 21:32:45 +0100

    chore(*): randomize class names between mfs

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 export default () => {
   return (
-    <div>
-      <h1> Container</h1>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
